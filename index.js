6fad464ffaac6455d188e221ec4cc122b5e5efa5
Notflavi <110347435+Notflavi@users.noreply.github.com>
2023-04-18 15:49:19 +0200

    Update files2

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const client = new Discord.Client(
     { intents: ["Guilds", "GuildMembers", "GuildMessages"] }
 )
 
-client.login("MTA4MjM4MzY2MzYxMTc4OTM5Mw.GnTM85.46IwMz8CiVsNtA0klSrVnPB4iusmXQABmk304c")
+client.login("process.env.token")
