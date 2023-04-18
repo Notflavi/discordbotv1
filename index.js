91688c1accb718f1f972e0fcdf5ce4de325d4968
Notflavi <110347435+Notflavi@users.noreply.github.com>
2023-04-18 15:45:57 +0200

    Upload Files

---
+++ index.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js")
+const client = new Discord.Client(
+    { intents: ["Guilds", "GuildMembers", "GuildMessages"] }
+)
+
+client.login("MTA4MjM4MzY2MzYxMTc4OTM5Mw.GnTM85.46IwMz8CiVsNtA0klSrVnPB4iusmXQABmk304c")
